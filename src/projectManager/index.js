bd9b5e94b402ee5950d806dd58c8e1cea230db46
vincentrodriguez <antoni.silvestrovic@gmail.com>
2019-03-18 18:20:01 +0000

    restore to previous version of project manager

---
+++ src/projectManager/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './index.scss';
+
+import ProjectManagerView from './components/ProjectManagerView/ProjectManagerView.jsx';
+
+render(<ProjectManagerView/>, document.getElementById('root'));
