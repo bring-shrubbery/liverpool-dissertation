0084e70390a82dab42688500e60200ee637d5fab
vincentrodriguez <antoni.silvestrovic@gmail.com>
2019-03-21 16:46:25 +0000

    docs update

--- docs_dev/blog.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Components
-import Blog from './components/Blog.jsx';
-
-ReactDOM.render(<Blog/>, document.getElementById("root"));
--- docs_dev/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Components
-import LandingPage from './components/LandingPage.jsx';
-
-ReactDOM.render(<LandingPage/>, document.getElementById("root"));
